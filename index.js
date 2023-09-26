65b0ecea20e10721f8f54568057f33c42b4aaec9
osmanandres35 <93053535+osmanandres35@users.noreply.github.com>
2023-09-26 11:23:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ const mysql = require('mysql2/promise');
 
 const app = express();
 
+// Permitir peticiones HTTP versión 1
+app.use((req, res, next) => {
+  req.httpVersion = '1.1';
+  req.httpVersionMajor = 1;
+  req.httpVersionMinor = 1;
+  next();
+});
 
 const pool = mysql.createPool({
   host: 'srv1075.hstgr.io',
   user: 'u945153519_mascotas',
   password: 'Eduar2532.',
   database: 'u945153519_mascotas',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 app.use(express.json());
